197187a8be5a8390e29fdfb1913bc486a24a1318
HuShiyangchn <55211567+HuShiyangchn@users.noreply.github.com>
2020-01-27 19:26:20 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,9 @@
 import Arabic2Roman as a2r
-import pytest
 
 def test():
-	assert a2r.Arabic2Roman(2)=="II"
-	assert a2r.Arabic2Roman(4)=="IV"
-	assert a2r.Arabic2Roman(9)=="IX"
-	assert a2r.Arabic2Roman(20)=="XX"
-	assert a2r.Arabic2Roman(58)=="LVIII"
-	assert a2r.Arabic2Roman(1994)=="MCMXCIV"
+	assert a2r.intToRoman(2)=="II"
+	assert a2r.intToRoman(4)=="IV"
+	assert a2r.intToRoman(9)=="IX"
+	assert a2r.intToRoman(20)=="XX"
+	assert a2r.intToRoman(58)=="LVIII"
+	assert a2r.intToRoman(1994)=="MCMXCIV"
